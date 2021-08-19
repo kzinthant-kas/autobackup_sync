3ac0e3ea327f3aeb263cd571638b0a2e747404fa
kzinthant-kas <88583919+kzinthant-kas@users.noreply.github.com>
2021-08-19 19:15:44 +0530

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -1,12 +1,8 @@
 import time
 import subprocess
 
-seconds_to_wait = 60
+seconds_to_wait = 360
 
 while(True):
   subprocess.call(['sh', './sync.sh'])
-  time.sleep(60)
-
-
-    
-  
+  time.sleep(seconds_to_wait)
